9e040b584c85e0ecb1aba7929661b53e30a26c89
JacopoTrincanato <trincanatoj@gmail.com>
2024-09-30 15:18:53 +0200

    create young_discount e senior_discount and give them values

--- main.js
+++ main.js
@@ -15,6 +15,12 @@ let price;
 //creo variabile dove salvare il prezzo del ticket
 let ticket;
 
+//creo variabile dove salvare lo sconto per i minorenni
+let young_discount;
+
+//creo variabile dove salvare lo sconto per gli over 65
+let senior_discount;
+
 //FASE 2: raccolta Dati
 //assegno un valore a kilometres
 kilometres = 100;
@@ -25,7 +31,18 @@ age = 30;
 //assegno un valore a price
 price = 0.21;
 
+//assegno un valore a young_discount
+young_discount = (price * kilometres *20) / 100;
+
+//assegno un valore a senior_discount
+senior_discount = (price * kilometres *40) / 100;
+
 //FASE 3: elaborazione dati
+//verifico il prezzo del biglietto per i minorenni
+if (first_word.length > second_word.length) {
+    message = "La prima parola è più lunga della seconda";
+
+}
 
 //FASE 4: output
 
